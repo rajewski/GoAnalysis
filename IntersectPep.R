2cceab5d7d5a629650b5ddc0f846a1fd65cddd78
rajewski <rajewski23@gmail.com>
2019-07-23 07:50:40 -0700

    add tomato comment

--- IntersectPep.R
+++ IntersectPep.R
@@ -1,7 +1,11 @@
 #to intersect a list of trnscripts of interest from a peptide list
+#this step and the blast/diamond step are unneccesary for the tomato datasets because
+#there is already a list of GO terms associated with with protein:
+#ITAG3.2_protein_go.tsv but it's format is slightly different than the Uniprot.tab sheet
 library(Biostrings)
+
 #Read in a list of all protein sequences
-Pros <- readAAStringSet("~/bigdata/Nobtusifolia/Genome_Files/NIOBT_r1.0.proteins.fa")
+NobtPro <- readAAStringSet("~/bigdata/Nobtusifolia/Genome_Files/NIOBT_r1.0.proteins.fa")
 #Read in a list of the query protein names
 wanted <- read.csv("~/bigdata/Nobtusifolia/RNA-seq/Results_Ballgown/Nobt_Prev3DPA_DEGs.csv")
 #strip off transcript suffix
