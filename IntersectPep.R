ffa7d644196633bc81567e8c313d62656581d6cb
rajewski <rajewski23@gmail.com>
2019-07-22 22:13:00 -0700

    comment out blast in favor of diamond

--- IntersectPep.R
+++ IntersectPep.R
@@ -4,12 +4,16 @@ library(Biostrings)
 Pros <- readAAStringSet("~/bigdata/Nobtusifolia/Genome_Files/NIOBT_r1.0.proteins.fa")
 #Read in a list of the query protein names
 wanted <- read.csv("~/bigdata/Nobtusifolia/RNA-seq/Results_Ballgown/Nobt_Prev3DPA_DEGs.csv")
-
 #strip off transcript suffix
 wanted$t_name <- substr(wanted$t_name,1,nchar(as.character(wanted$t_name))-3)
 
-#intersect the two list
-matches <- Pros[match(wanted$t_name,names(Pros))]
+#split into up and down regulated
+Up <- wanted[wanted$fc>=1,]
+Down <- wanted[wanted$fc<1,]
 
+#intersect the two list
+UpMatch <- Pros[match(Up$t_name,names(Pros))]
+DownMatch <- Pros[match(Down$t_name,names(Pros))]
 #write it to a fasta file
-writeXStringSet(matches, "DEG_pep.fasta", format = "fasta")
+writeXStringSet(UpMatch, "Up.fasta", format = "fasta")
+writeXStringSet(DownMatch, "Down.fasta", format = "fasta")
